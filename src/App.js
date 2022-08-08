e2b97d75660a299cfa2dcda0e46a82a6f49cccdf
Roberto Di Capua <dicapuaroberto@outlook.it>
2022-08-08 18:14:50 +0200

    front end calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from 'react';
+import classes from './App.module.css';
 
+function reducer(state, {type, payload}) {
+  
+}
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes['calculator-grid']}>
+        <div className={classes.output}>
+          <div className={classes['previus-operand']}>{previousOperand} {operation}</div>
+          <div className={classes['current-operand']}>{currentOperand}</div>
+        </div>
+        <button className={classes['span-two']}>AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className={classes['span-two']}>=</button>
     </div>
   );
 }
